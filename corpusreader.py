04279feff5c7a4fcb0deed2371d709a1af05cf81
wanneboveneaard <wannebovendeaard@gmail.com>
2025-05-08 12:48:40 +0200

    modules aangemaakt

---
+++ corpusreader.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  8 12:39:27 2025
+
+@author: wanne
+"""
+
---
+++ model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  8 12:40:00 2025
+
+@author: wanne
+"""
+
