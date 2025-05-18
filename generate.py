722b34d1f1599a857915da563059f79aa7dbef56
wanneboveneaard <wannebovendeaard@gmail.com>
2025-05-18 22:04:41 +0200

    voor jacob

--- generate.py
+++ generate.py
@@ -1,7 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  8 12:40:26 2025
+def generate_sentence(ngram_model):
+    n = ngram_model.n
+    generated_sentence = ["<s>" * (n-1)]
+    
+    while True:
+        prefix = generated_sentence[-(n - 1):]
+        next_word = ngram_model.choose_successor(prefix)
+    
+        if next_word is None or "</s>":
+            break
+        
+        generated_sentence.append(next_word)
+    
+    return generated_sentence
+    
+#Hier begint de executable sectie
+if __name__ == "__main__":
+    # Lees pad naar corpus en n-waarde uit command line
+if len(sys.argv) > 1:
+    = sys.argv[1]
+        else:
+        corpus_path = "./train"
 
-@author: wanne
-"""
+    if len(sys.argv) > 2:
+        n = int(sys.argv[2])
+    else:
+        n = 2
 
+    # Stap 2: Lees corpus en maak model
+    corpus = CorpusReader(corpus_path)
+    sentences = corpus.sents()
+    model = NgramModel(sentences, n)
+
+    # Stap 3: Genereer en print twee zinnen
+    print("> " + " ".join(generate))
--- model.py
+++ model.py
@@ -1,131 +1,107 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  8 12:40:00 2025
-
-@author: wanne
-"""
-print("Hoi")
-
 import re
 from corpusreader import CorpusReader
 import random
+import math
 
 class NgramModel:
     
     def __init__(self, tokenized_sentences, n):
-        """accepts a list of tokenized sentences and ...?"""
-        """self.tokenized_sentences = tokenized_sentences #  property
-    
-        self.clean_sentences = []
-
-        for sentence in self.tokenized_sentences:
-            self.words_in_sentence = [n*"<s>"]
-            for word in sentence:
-                if re.search(r"\b\w*\b", word): # \W stat voor letters
-                    self.words_in_sentence.append(word.lower())
-            self.words_in_sentence.append("</s>")
-            self.clean_sentences.append(self.words_in_sentence)""" "even comment van gemaakt"
-
+        """accepts a list of tokenized sentences and builds n-gram model"""
+        self.n = n
         
         self.ngram_counts = {}
         self.prefix_counts = {}
-        self.tokenized_sentences = tokenized_sentences #  property
+        self.tokenized_sentences = tokenized_sentences
 
         self.clean_sentences = []
         
         for sentence in self.tokenized_sentences:
-            self.words_in_sentence = (n-1) *["<s>"]
+            words_in_sentence = (n - 1) * ["<s>"]
             for word in sentence:
-                if re.search(r"\b\w*\b", word): # \W stat voor letters
-                    self.words_in_sentence.append(word.lower())
-        self.words_in_sentence.append("</s>")
-        self.clean_sentences.append(self.words_in_sentence)
-    
-        #self.maak_freq_tab() # is dit nodig?
+                if re.search(r"\b\w+\b", word):
+                    words_in_sentence.append(word.lower())
+            words_in_sentence.append("</s>")
+            self.clean_sentences.append(words_in_sentence)
 
+        self.vocabulary = set()
+        self.context_freq = {}
+        self.ngram_freq = {}
+        self.maak_freq_tab()
     
-    def maak_freq_tab(self, clean_sentences): # maakt een frequency tabel
-        freq_dict = dict()
-        
+    def maak_freq_tab(self): 
         for sentence in self.clean_sentences:
-            for i in range(len(sentence) - self.n - 1):
-                    prefix = tuple(sentence[i:i+ (self.n - 1)])
-                    next_word = sentence[i+self.n - 1]
-                    #ngram = tuple(sentence[i:i+self.n])
-                    if prefix not in self.ngram_counts:
-                        self.ngram_counts[prefix] = {}
-                    
-                    if next_word not in self.ngram_counts[prefix]:
-                        self.ngram_counts[prefix][next_word] = 0
-                    
-                    self.ngram_counts[prefix][next_word] += 1
-                    
-                    if prefix not in self.prefix_counts:
-                        self.prefix_counts[prefix] = 0
-                    
-                    self.prefix_counts[prefix] += 1
-                
-        return freq_dict
+            for i in range(len(sentence) - self.n + 1):
+                prefix = tuple(sentence[i:i + (self.n - 1)])
+                next_word = sentence[i + self.n - 1]
+                ngram = prefix + (next_word,)
 
+                self.vocabulary.add(next_word)
+
+                self.ngram_freq[ngram] = self.ngram_freq.get(ngram, 0) + 1
+
+                self.context_freq[prefix] = self.context_freq.get(prefix, 0) + 1
+
+                # Voor successors
+                if prefix not in self.ngram_counts:
+                    self.ngram_counts[prefix] = {}
+                if next_word not in self.ngram_counts[prefix]:
+                    self.ngram_counts[prefix][next_word] = 0
+                self.ngram_counts[prefix][next_word] += 1
+
+                # Ook tellen in prefix_counts
+                self.prefix_counts[prefix] = self.prefix_counts.get(prefix, 0) + 1
+    
     def probability(self, ngram, smoothing_constant=0.0):
-        
-        #V = len(set(clean_sentences)) # verzameling unieke woorden (voor smoothing)
-
         for word in ngram:
             if word not in self.vocabulary:
                 return 0.0
         
-        # Extract context (first n-1 words) and target word (last word)
-        context = ngram[:-1]
+        context = tuple(ngram[:-1])
         target_word = ngram[-1]
-        
-        # Get counts
-        ngram_count = self.ngram_freq.get(ngram, 0)
+        full_ngram = context + (target_word,)
+
+        ngram_count = self.ngram_freq.get(full_ngram, 0)
         context_count = self.context_freq.get(context, 0)
-        
-        # Handle case where context was never seen
+
         if context_count == 0:
             return 0.0
-        
-        # Calculate probability based on smoothing
+
         if smoothing_constant == 0.0:
-            # Raw probability: P(target | context) = count(ngram) / count(context)
             return ngram_count / context_count
         else:
-            # Add-k smoothed probability: P(target | context) = (count(ngram) + k) / (count(context) + k * V)
             k = smoothing_constant
             V = len(self.vocabulary)
             return (ngram_count + k) / (context_count + k * V)
         
     def perplexity(self, sentence, smoothing_constant=1.0):
-        
-        clean_sentence = (self.n - 1) * [self.n-1]
-        
-        for word in clean_sentence:
-            if re.search(r"\b\w*\b", word): # \W stat voor letters
-                self.clean_sentence.append(word.lower())
-                self.clean_sentence.append("</s>")
-                
-        if len(self.cleaned_sentences) < self.n:
+        clean_sentence = (self.n - 1) * ["<s>"]
+        for word in sentence:
+            if re.search(r"\b\w+\b", word):
+                clean_sentence.append(word.lower())
+        clean_sentence.append("</s>")
+
+        if len(clean_sentence) < self.n:
             return float("inf")
+
+        log_prob_sum = 0.0
+        for i in range(len(clean_sentence) - self.n + 1):
+            ngram = clean_sentence[i:i + self.n]
+            prob = self.probability(ngram, smoothing_constant)
+            if prob == 0.0:
+                return float("inf")
+            log_prob_sum += -1 * (math.log(prob, 2))
         
-        for i in range(len(self.cleaned_sentences) - self.n +1):
-            ngram = tuple(self.cleaned_sentences[i:i + self.n])
-        
-        prob = self.probability(list(ngram), smoothing_constant)
-        
-        if prob == 0.0:
-            return float("inf")
-        
+        return 2 ** (log_prob_sum / len(clean_sentence))
+
     def choose_succesor(self, prefix):
         prefix_tuple = tuple(word.lower() for word in prefix)
         if len(prefix_tuple) != self.n - 1:
             raise ValueError("Prefix heeft verkeerde lengte")
 
-        if prefix_tuple not in self.ngram:
+        if prefix_tuple not in self.ngram_counts:
             return None
         
-        successors_dict = self.ngram[prefix_tuple]
+        successors_dict = self.ngram_counts[prefix_tuple]
         if not successors_dict:
             return None
         
@@ -134,10 +110,8 @@ class NgramModel:
         
         chosen_word = random.choices(words, weights=weights, k=1)[0]
         return chosen_word
-                
-corpus = CorpusReader(r"C:\Users\wanne\Downloads\Computational Linguistics\small-corpus")
-                  
-lol = NgramModel(corpus.sents(), 2)
-#print(lol.clean_sentences[-100:])
-print(lol.maak_freq_tab(lol.clean_sentences))
 
+# === TESTEN ===
+corpus = CorpusReader(r"C:\Users\wanne\Downloads\Computational Linguistics\small-corpus")
+lol = NgramModel(corpus.sents(), 2)
+print(lol.ngram_counts)
