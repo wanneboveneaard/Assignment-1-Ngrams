9fc8bcfab4c4e3e0e7565a8ac0344586548421ad
wanneboveneaard <wannebovendeaard@gmail.com>
2025-05-18 22:14:49 +0200

    voor Mela

--- generate.py
+++ generate.py
@@ -1,24 +1,29 @@
+
+    
+from corpusreader import CorpusReader
+from model import NgramModel
+import sys
+
 def generate_sentence(ngram_model):
     n = ngram_model.n
     generated_sentence = ["<s>" * (n-1)]
-    
+
     while True:
         prefix = generated_sentence[-(n - 1):]
         next_word = ngram_model.choose_successor(prefix)
-    
         if next_word is None or "</s>":
             break
-        
         generated_sentence.append(next_word)
-    
+
     return generated_sentence
-    
-#Hier begint de executable sectie
+
+
+#executable section
 if __name__ == "__main__":
-    # Lees pad naar corpus en n-waarde uit command line
-if len(sys.argv) > 1:
-    = sys.argv[1]
-        else:
+    # Stap 1: Lees pad naar corpus en n-waarde uit command line
+    if len(sys.argv) > 1:
+        corpus_path = sys.argv[1]
+    else:
         corpus_path = "./train"
 
     if len(sys.argv) > 2:
@@ -32,4 +37,22 @@ if len(sys.argv) > 1:
     model = NgramModel(sentences, n)
 
     # Stap 3: Genereer en print twee zinnen
-    print("> " + " ".join(generate))
+    print("> " + " ".join(generate_sentence(model)))
+    print()
+    print("> " + " ".join(generate_sentence(model)))
+
+    # Stap 4: Bereken perplexities van gegeven zinnen
+    print("\nSentence perplexities:\n")
+
+    test_sentences = [
+        "Suggestive, Watson, is it not?",
+        "It is amazing that a family can be torn apart by something as simple as a pack of wild dogs!",
+        "So spoke Sherlock Holmes and turned back to the great scrapbook in which he was arranging and indexing some of his recent material.",
+        "What I like best about my friends is that they are few.",
+        "Friends what is like are they about I best few my that."
+    ]
+
+    for sent in test_sentences:
+        tokens = nltk.word_tokenize(sent)
+        perplexity = model.perplexity(tokens)
+        print(f'"{sent}" -> Perplexity: {perplexity}')
